3fc76c8d6094033d5034217eed1ff5a951e56efa
Paulo Serpa Antunes <pauloserpaantunes@gmail.com>
2019-10-12 16:44:46 -0300

    Consertado erro de JS; removidos arquivos CSS

--- js/main.js
+++ js/main.js
@@ -158,7 +158,3 @@ $('.open-popup-link').magnificPopup({
   type:'iframe',
   midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 });
-
-document.getElementById("botaotemp1").onclick = function () {
-        location.href = "result.html";
-    };
